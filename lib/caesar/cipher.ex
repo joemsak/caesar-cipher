7df3ed17b072947a059a6b405c9f0f6a93d3a6f4
Joe Sak <joe@joesak.com>
2016-06-07 10:06:07 -0500

    Refactor and add docs

--- lib/caesar/cipher.ex
+++ lib/caesar/cipher.ex
@@ -1,5 +1,22 @@
 defmodule Caesar.Cipher do
+  require Logger
+
+  @alphabet_size Application.get_env(:caesar, :alphabet_size)
+
+  @moduledoc """
+  Caesar Cipher module, which maps characters to new characters.
+  """
+
+  @doc """
+  Encrypt the message by applying a mapping that shifts the alphabet by the `shift` value
+
+  ## Examples
+
+      iex> Caesar.Cipher.encrypt("hello world!", 1)
+      "gdkkn vnqkc!"
+  """
   def encrypt(msg, shift) do
+    Logger.debug("encrypting \"#{msg}\" with a shift number: #{shift}")
     # convert msg to char_list
     msg
       |> to_char_list
@@ -20,10 +37,10 @@ defmodule Caesar.Cipher do
   defp calculate_mapping(base_letter, char, shift) do
     # find the ASCII integer of the char and normalize it
     # by subtracting the size of the alphabet
-    normalize = &(&1 - 26)
+    normalize = &(&1 - @alphabet_size)
     # ensure shift number is within the alphabet size
-    shift_num = rem(shift, 26)
+    shift_num = rem(shift, @alphabet_size)
     # calculate the shifted value
-    base_letter + rem(char - normalize.(base_letter) - shift_num, 26)
+    base_letter + rem(char - normalize.(base_letter) - shift_num, @alphabet_size)
   end
 end
